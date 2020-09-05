1a382fc7ab2e87a7e768dc73b472087f2eefa53a
neilbox1 <neilgonzalez22@csu.fullerton.edu>
2020-09-05 14:10:02 -0700

    build update

--- build-client.cmd
+++ build-client.cmd
@@ -1,4 +1,5 @@
-go get github.com/gonutz/w32 REM fixed procRedrawWindow for 64 bit
+REM fixed procRedrawWindow for 64 bit
+go get github.com/gonutz/w32
 go get github.com/NebulousLabs/go-upnp
 go get golang.org/x/sys/windows/registry
 go get github.com/StackExchange/wmi
